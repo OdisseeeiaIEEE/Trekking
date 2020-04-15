f071aacd319544f0b643000b88b0db53cb5e1249
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2020-04-15 08:04:12 -0300

    comunicaçao serial init

--- firmware-trekking/Omnius.h
+++ firmware-trekking/Omnius.h
@@ -1,16 +1,23 @@
-class motor
+#include "Arduino.h"
+
+/*********************************/
+/*              Motores          */
+/*********************************/
+
+class Motor
 {
   public:
-    motor(int pinA, int pinB);
+    Motor(int pinA, int pinB);
 
+    void iniciar(int pinA, int pinB);
     void frente();
     void re();
 
   private:
-    int _pinA, _pinB;
+    int _pinA, _pinB; //precisa dessas var?
 };
 
-motor::motor(int pinA, int pinB)
+Motor::Motor(int pinA, int pinB)
 {
   pinMode(pinA, OUTPUT);
   pinMode(pinB, OUTPUT);
@@ -18,12 +25,59 @@ motor::motor(int pinA, int pinB)
   _pinB = pinB;
 }
 
-void motor::frente ()
+void Motor::iniciar(int pinA, int pinB)
+{
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  _pinA = pinA;
+  _pinB = pinB;
+}
+
+void Motor::frente ()
 {
 
 }
 
-void motor::re ()
+void Motor::re ()
 {
 
+}
+
+
+/*********************************/
+/*       Comunicação Serial      */
+/*********************************/
+
+/*
+A fução vai retornar true or false se encontrar o dado que se procura.
+e retornar seu valor.
+
+"tipo"="valor";
+*/
+
+class serial
+{
+  public:
+    serial(int baud_rate);
+
+    void main();
+    char r; //armazenar char lido
+    string reading; //armazenar array char lido
+
+  private:
+};
+
+serial::serial(int baud_rate)
+{
+  Serial.begin(baud_rate);
+}
+
+serial::main()
+{
+  whilea(Serial.available > 0)
+  {
+    r = Serial.read();
+
+    //se r for igual a ";" significa que a leitura foi terminada
+  }
 }
--- firmware-trekking/firmware-trekking.ino
+++ firmware-trekking/firmware-trekking.ino
@@ -5,10 +5,15 @@
 
 *https://www.arduino.cc/en/Hacking/libraryTutorial
 *https://playground.arduino.cc/Code/Struct/
+*https://www.inf.pucrs.br/~pinho/PRGSWB/OO/oocpp.html
 */
+
+#define motor_Pin_A 11   //verificar pinagem!
+#define motor_Pin_B 12
 
 #include "Omnius.h"
 
+//void Motor::iniciar(15, 16); //nao funciona mas pq?
 
 void setup ()
 {
